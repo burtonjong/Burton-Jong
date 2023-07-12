9c90876408a3e8bde15c01ffdf88870cf71789ac
Burton Jong <burtonjong05@gmail.com>
2023-07-11 22:07:45 -0600

    Test 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bark bark woof...</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div>Hello</div>;
 }
-
-export default App;
