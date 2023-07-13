421b939b30af17c5d929a3e8495564d13882c98d
Burton Jong <burtonjong05@gmail.com>
2023-07-12 21:48:03 -0600

    Deletion of unused files, setting up background for the title and the body of the website.

---
+++ src/Components/Expereience.js
@@ -0,0 +1,7 @@
+export default function Experience() {
+  return (
+    <div className="spacer layer2">
+      <section></section>
+    </div>
+  );
+}
---
+++ src/Components/Title.js
@@ -0,0 +1,11 @@
+export default function Title() {
+  return (
+    <div className="spacer layer1">
+      <section>
+        <div>
+          <h1>Burton Jong</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
