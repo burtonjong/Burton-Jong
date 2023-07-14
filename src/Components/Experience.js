36bba96ecebf1ef5c471a73d037790c33e128461
Burton Jong <burtonjong05@gmail.com>
2023-07-14 17:46:38 -0600

    Home page change to a loading screen.

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,7 +2,7 @@ import Button1 from "./Button1";
 
 export default function Experience() {
   return (
-    <div className="container jc-center">
+    <div className="jc-center">
       <ul className="icon-links-ul fd-column">
         <li className="no-bullets underlined animate slide-in-right slow-7">
           <Button1 />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Title from "./Title";
 import Links from "./Links";
 import Experience from "./Experience";
+import LoadingBar from "./LoadingBar";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <div className="container spacer layer1">
-        <div className="container-content">
-          <div className="container-left"></div>
+  const [showComponent, setShowComponent] = useState(false);
+  const [timer1, setTimer1] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer1(!timer1);
+    }, 1000);
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowComponent(!showComponent);
+    }, 5000);
+  }, []);
 
-          <div className="container-right">
-            <Links />
-            <Experience />
-          </div>
+  return (
+    <div className="container-stay">
+      <div
+        className={"container-fade " + (showComponent ? "animate fade" : "")}
+      >
+        <div
+          className={
+            "profile-container " + (timer1 ? "animate fade slow-8" : "")
+          }
+        >
+          <div className="loader"></div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
