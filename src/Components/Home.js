d9cd38910fbca3ba05006955d55a382ccc446b83
Burton Jong <burtonjong05@gmail.com>
2023-07-14 18:31:08 -0600

    Large change. Loading bar complete, new title.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,32 +6,30 @@ import LoadingBar from "./LoadingBar";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [showComponent, setShowComponent] = useState(false);
-  const [timer1, setTimer1] = useState(false);
+  const [timer2, setTimer2] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-      setTimer1(!timer1);
-    }, 1000);
+      setTimer2(!timer2);
+    }, 7500);
   }, []);
-  useEffect(() => {
-    setTimeout(() => {
-      setShowComponent(!showComponent);
-    }, 5000);
-  }, []);
-
   return (
     <div className="container-stay">
-      <div
-        className={"container-fade " + (showComponent ? "animate fade" : "")}
-      >
-        <div
-          className={
-            "profile-container " + (timer1 ? "animate fade slow-8" : "")
-          }
-        >
-          <div className="loader"></div>
+      {timer2 && <LoadingBar />}
+      {!timer2 && (
+        <div className="container-home">
+          <div className="container-title">
+            <h1 className="animate fade-up slow-1">BURTON JONG</h1>
+            <div className="container-content">
+              <h2 className="animate fade-up slow-2">
+                I am a second year student in the{" "}
+                <span className="keywords">Shulich School of Engineering</span>{" "}
+                pursuing my Bachelor's in{" "}
+                <span className="keywords">Software Engineering</span>.
+              </h2>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
