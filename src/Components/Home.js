6079d442b14ea6b32230ef697505d28cbdd05e6a
Burton Jong <burtonjong05@gmail.com>
2023-07-15 16:31:44 -0600

    Lots of media query changes.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import Title from "./Title";
-import Links from "./Links";
+import Navbar from "./Navbar";
 import Experience from "./Experience";
 import LoadingBar from "./LoadingBar";
 import { useState, useEffect } from "react";
@@ -13,23 +12,29 @@ export default function Home() {
     }, 5500);
   }, []);
   return (
-    <div className="container-stay">
-      {timer2 && <LoadingBar />}
-      {!timer2 && (
-        <div className="container-home">
-          <div className="container-title">
-            <h1 className="animate fade-up slow-1">BURTON JONG</h1>
-            <div className="container-content">
-              <h2 className="animate fade-up slow-2">
-                I am a second year student in the{" "}
-                <span className="keywords">Shulich School of Engineering</span>{" "}
-                pursuing my Bachelor's in{" "}
-                <span className="keywords">Software Engineering</span>.
-              </h2>
+    <>
+      <div className="container-stay">
+        {timer2 && <LoadingBar />}
+        {!timer2 && (
+          <div className="container-home">
+            <Navbar />
+            <div className="container-title">
+              <h1 className="animate fade-up slow-1">BURTON JONG</h1>
+              <div className="container-content">
+                <h2 className="animate fade-up slow-2">
+                  I am a second year student in the{" "}
+                  <span className="keywords">
+                    Shulich School of Engineering
+                  </span>{" "}
+                  pursuing my Bachelor's in{" "}
+                  <span className="keywords">Software Engineering</span>.
+                </h2>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      <Experience />
+    </>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <div class="fixed-navbar"></div>;
+}
