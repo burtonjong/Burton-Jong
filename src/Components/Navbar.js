032b615bc239a930095f2615a312f7eef57bcb00
Burton Jong <burtonjong05@gmail.com>
2023-07-15 21:10:57 -0600

    Navbar added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,27 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 export default function Navbar() {
-  return <div class="fixed-navbar"></div>;
+  return (
+    <div class="fixed-navbar">
+      <ul>
+        <a href="www.google.com">
+          <li className="navbar-item animate fade-down slow-3">// Home //</li>
+        </a>
+        <a href="www.google.com">
+          <li className="navbar-item animate fade-down slow-3">
+            // Expertise //
+          </li>
+        </a>
+        <a href="www.google.com">
+          <li className="navbar-item animate fade-down slow-3">
+            // Projects //
+          </li>
+        </a>
+        <a href="www.google.com">
+          <li className="navbar-item animate fade-down slow-3">
+            // Contact //
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
 }
